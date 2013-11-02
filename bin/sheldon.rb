bff94e7e6bef495297c77572847cdbf013dcecc2
Dave  Jones <dave@Daves-MacBook-Pro.local>
2013-11-02 18:30:47 +0000

    Renaming directory so Sheldon can be smarter

--- bin/sheldon.rb
+++ bin/sheldon.rb
@@ -5,16 +5,18 @@
 
 require 'FileUtils'
 
-target_dir = Dir.pwd
-collection_dir = File.expand_path("../../#{ARGV[0]}",__FILE__)
+target_path = Dir.pwd
+target_dir = ARGV[0] || Dir.getwd
+puts target_dir
+collection_dir = File.expand_path("../../#{target_dir}",__FILE__)
 
-Dir.entries(collection_dir).each do |entry|
-  source_path =  File.join(collection_dir,entry)
-  if File.file?(source_path)
-    puts "Symlink #{entry}?"
-    answer = STDIN.gets.chomp
-    if answer.downcase == "y"
-      FileUtils.ln_s(source_path, target_dir, :force=>true)
-    end
-  end
-end
+# Dir.entries(collection_dir).each do |entry|
+#   source_path =  File.join(collection_dir,entry)
+#   if File.file?(source_path)
+#     puts "Symlink #{entry}?"
+#     answer = STDIN.gets.chomp
+#     if answer.downcase == "y"
+#       FileUtils.ln_s(source_path, target_path, :force=>true)
+#     end
+#   end
+# end
