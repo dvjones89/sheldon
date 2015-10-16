d9547517dc39d2555e9c7442e6633a8bc7b4a763
Dave Jones <david@dvj.me.uk>
2015-10-16 19:01:01 +0100

    Sheldon can now learn about new files and directories, move them into his brain and symlink back to the original directory

--- bin/sheldon.rb
+++ bin/sheldon.rb
@@ -42,8 +42,15 @@ class Sheldon
           puts "Name not specified or already in use. Please try again."
           abort
         else
-          FileUtils.mkdir_p(abs_brain_path)
-          FileUtils.cp_r(abs_learn_path, File.join(abs_brain_path,'..')) # The '..' is required here else you duplicate the root
+          if File.file?(abs_learn_path)
+            dest_path = File.dirname(abs_brain_path) # If we're learning about a file, just create and move to the parent director(ies)
+          else
+            dest_path = File.join(abs_brain_path, '..') # If we're learning about a directory, we want to create parent director(ies) and drop the new file at the leaf
+          end
+
+          FileUtils.mkdir_p(dest_path)
+          FileUtils.mv(abs_learn_path, dest_path)
+          FileUtils.ln_s(abs_brain_path, abs_learn_path)
           database[friendly_name] = {file_path: rebase_from_home(abs_learn_path), brain_path: rebase_from_home(abs_brain_path)}
         end
       end
