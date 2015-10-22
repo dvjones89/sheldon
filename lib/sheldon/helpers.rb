bcc62f8d4d21a721b3ca8f1a928e3632ba4bc530
Dave Jones <david@dvj.me.uk>
2015-10-22 19:39:43 +0100

    Happy Hound: Prefer double quotes to single quotes. Remove methods that have been moved into

--- lib/sheldon/helpers.rb
+++ lib/sheldon/helpers.rb
@@ -1,28 +1,17 @@
 module Helpers
 
-  def locate_brain
-    relative_path = '~/sheldon2'
-    # relative_path = ENV['SHELDON_DATA_DIR'] || '~/sheldon'
-    Pathname(relative_path).expand_path # Deals with the use of ~ when referencing home directories
-  end
-
   def load_db
-    YAML::Store.new(File.join(locate_brain, 'db.yaml'))
+    YAML::Store.new(File.join(locate_brain, "db.yaml"))
   end
 
   def remove_home(path)
-    home_path = Pathname(File.expand_path('~'))
+    home_path = Pathname(File.expand_path("~"))
     Pathname(path).relative_path_from(home_path).to_s
   end
 
   def add_home(path)
-    abs_home = File.expand_path('~')
+    abs_home = File.expand_path("~")
     File.join(abs_home,path).to_s
-  end
-
-  def find_synapse(friendly_name,rel_learn_path)
-    basename = File.basename(rel_learn_path)
-    File.join(locate_brain,friendly_name,basename)
   end
 
 end
