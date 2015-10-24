ab3a7ea781416da2cc5864019a0de4559aa72ead
Dave Jones <david@dvj.me.uk>
2015-10-24 16:57:37 +0100

    Further Refactoring   * Prompt for user input now originates from the `Launcher` class   * Removing broken commands from `Launcher`. These will be reimplemented one by one.   * The launcher now uses a memoized instance of `lib/sheldon.rb`

--- lib/sheldon.rb
+++ lib/sheldon.rb
@@ -3,15 +3,8 @@ require_relative 'sheldon/brain'
 
 class Sheldon
 
-  def test(recall_cue)
-    puts brain.memory.has_cue?(recall_cue)
-  end
-
-  def learn(rel_learn_path)
+  def learn(recall_cue, rel_learn_path)
     abs_learn_path = File.join(Dir.pwd, rel_learn_path)
-  
-    print("Friendly Name For File/Folder: ")
-    recall_cue = STDIN.gets.chomp
 
     if brain.memory.has_cue?(recall_cue)
       raise "This cue has already been used. Please provide another."
@@ -22,9 +15,10 @@ class Sheldon
   end
 
   private
-    def brain
-      sheldon_data_dir = File.expand_path("~/sheldon2")
-      @brain ||= Brain.new(sheldon_data_dir)
-    end
+
+  def brain
+    sheldon_data_dir = File.expand_path("~/sheldon2")
+    @brain ||= Brain.new(sheldon_data_dir)
+  end
 
 end
