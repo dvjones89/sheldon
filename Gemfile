66fa5a468dd0c155fd854cead88ded16d3ec98e9
Dave Jones <david@dvj.me.uk>
2015-10-25 17:28:41 +0000

    Adding simplecov to measure code coverage.

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source "https://rubygems.org"
 gem "byebug"
 gem "mocha"
 gem "rspec"
+gem 'simplecov', :require => false, :group => :test
 gem "thor"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+# This must remain as the first import, before any application code is required.
+require "simplecov"
+SimpleCov.start
+
 Dir["#{File.dirname(__FILE__)}/../lib/*/*.rb"].each { |f| require f }
 require_relative "../lib/sheldon"
 
