a40086113f973fcc568bc94640b0d731f06bd6de
Dave Jones <david@dvj.me.uk>
2015-11-10 22:13:08 +0000

    Refactor Sheldon#build and start work on spec(s)

--- lib/sheldon/builder.rb
+++ lib/sheldon/builder.rb
@@ -3,8 +3,13 @@ class Builder
 
   def build(abs_build_path)
     entries = Dir.glob(abs_build_path + "/*")
-    buffer = entries.inject("") do |buffer, entry|
+    master_content = entries.inject("") do |buffer, entry|
       is_config?(entry) ? add_entry_to_buffer(entry, buffer) : buffer
+    end
+    
+    unless master_content.empty?
+      master_path = File.join(abs_build_path, "config")
+      File.open(master_path, "w") { |f| f.write(master_content) }
     end
   end
 
@@ -12,12 +17,12 @@ class Builder
 
   def add_entry_to_buffer(abs_path, buffer)
     content = File.read(abs_path)
-    buffer += content + "\n"
+    buffer + content + "\n"
   end
 
   def is_config?(abs_path)
     basename = File.basename(abs_path)
-    return true if File.file?(abs_path) && basename.include?('config_')
+    return true if File.file?(abs_path) && basename.include?("config_")
     false
   end
 
---
+++ spec/sheldon/builder_spec.rb
@@ -0,0 +1,5 @@
+require "spec_helper"
+
+describe Builder do
+  
+end
