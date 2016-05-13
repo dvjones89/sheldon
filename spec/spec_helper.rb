5e68fb82451af886bbba473972bb7af7941e52b5
Dave Jones <david@dvj.me.uk>
2016-05-13 18:45:53 +0100

    Re-ordering spec helper so that code-coverage calculations are accurate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,14 @@
-require "byebug"
 require 'coveralls'
 require "rspec"
 require "simplecov"
-require_relative "../lib/sheldon"
 
 # # This must remain as the first import, before any application code is required.
 SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
   add_filter "/spec/"
 end
+
+# This must remain below the SimpleCov declarations, else code coverage calculations are inaccurate
+require_relative "../lib/sheldon"
+
 
