c40caf91959edeeff0245dcd4be7c848682e0edf
Dave J <david@dvj.me.uk>
2018-03-20 20:24:46 +0000

    Order specs alphabetically

--- spec/sheldon/sheldon_spec.rb
+++ spec/sheldon/sheldon_spec.rb
@@ -42,11 +42,29 @@ describe Sheldon do
   end
 
   # Smoke-test
+  describe "#forget" do
+    context "for a cue that exists in Sheldon's memory" do
+      it "should forget the cue successfully" do
+        sheldon.learn("my git config", abs_learn_path)
+        expect(sheldon.forget("my git config")).to be true
+      end
+    end
+  end
+
+  # Smoke-test
   describe "#learn" do
     context "for a new cue that does not exist in Sheldon's memory" do
       it "should learn the new cue successfully" do
         expect(sheldon.learn("my git config", abs_learn_path)).to be true
       end
+    end
+  end
+
+  # Smoke-test
+  describe "#list_cues" do
+    it "should respond with the list of learned cues" do
+      sheldon.learn("my git config", abs_learn_path)
+      expect(sheldon.list_cues).to eq ["my git config"]
     end
   end
 
@@ -61,30 +79,12 @@ describe Sheldon do
   end
 
   # Smoke-test
-  describe "#forget" do
-    context "for a cue that exists in Sheldon's memory" do
-      it "should forget the cue successfully" do
-        sheldon.learn("my git config", abs_learn_path)
-        expect(sheldon.forget("my git config")).to be true
-      end
-    end
-  end
-
-  # Smoke-test
   describe "#recalled?" do
     context "for a cue that has been recalled" do
       it "should return true" do
         sheldon.learn("my git config", abs_learn_path)
         expect(sheldon.recall("my git config")).to be true
       end
-    end
-  end
-
-  # Smoke-test
-  describe "#list_cues" do
-    it "should respond with the list of learned cues" do
-      sheldon.learn("my git config", abs_learn_path)
-      expect(sheldon.list_cues).to eq ["my git config"]
     end
   end
 
