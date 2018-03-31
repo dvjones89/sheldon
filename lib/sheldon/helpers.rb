ab331c75cd803284870b515238c358c74acffa62
Dave J <david@dvj.me.uk>
2018-03-31 12:05:17 +0100

    Add support for writing backtrace to log file. Fixed #18 Move helper methods from CLI class to Helpers module, soon to be tested

--- lib/sheldon/helpers.rb
+++ lib/sheldon/helpers.rb
@@ -1,9 +1,8 @@
 module Helpers
   require "pathname"
 
-  def remove_home(path)
-    home_path = Pathname(File.expand_path("~"))
-    Pathname(path).relative_path_from(home_path).to_s
+  def abs(rel_path)
+    File.expand_path(rel_path).to_s
   end
 
   def add_home(path)
@@ -11,8 +10,35 @@ module Helpers
     File.join(abs_home, path).to_s
   end
 
-  def abs(rel_path)
-    File.expand_path(rel_path).to_s
+  def read_from_dotfile(key)
+    dotfile = YAML::Store.new(add_home(".sheldon"))
+    dotfile.transaction { dotfile[key] }
+  end
+
+  def remove_home(path)
+    home_path = Pathname(File.expand_path("~"))
+    Pathname(path).relative_path_from(home_path).to_s
+  end
+
+  def with_exception_handling(&block)
+    yield
+  rescue Exception => e
+    red(e.message)
+
+    if debug_file_path = read_from_dotfile("debug_file_path")
+      log_msg = ([Time.now.to_s] + [e.message] + e.backtrace).join("\n")
+      File.open(debug_file_path, 'a') { |f| f.write(log_msg + "\n\n") }
+      red("Stack trace written to #{debug_file_path}")
+    else
+      red("To capture a stack trace, please set debug_file_path in ~/.sheldon")
+    end
+
+    exit(1)
+  end
+
+  def write_to_dotfile(key, value)
+    dotfile = YAML::Store.new(add_home(".sheldon"))
+    dotfile.transaction { @dotfile[key] = value }
   end
 
 end
